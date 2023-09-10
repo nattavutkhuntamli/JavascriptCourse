9d0b10785b12dce930e0bd4e75fb1094415b4759
DESKTOP-IL0ECE0\bentley <nattavutkhuntamli@gmail.com>
2023-09-10 19:43:43 +0700

    ปรับพื้นฐานการใช้ ejs ทำระบบแสดงรายการสินค้า

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,12 @@ exports.getaddProduct = (req, res, next) => {
   
   exports.postAddProduct = (req, res, next) => {
     // products.push({ title: req.body.title });
-    const product = new Product(req.body.title);
+    console.log(req.body);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title,imageUrl,price,description);
     product.save();
     res.redirect("/");
   };
